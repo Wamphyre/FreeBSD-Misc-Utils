f74f812c86a5d7511bac907620f136451c09d822
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2018-02-06 18:31:52 +0100

    Updated

--- autowordpress.sh
+++ autowordpress.sh
@@ -9,6 +9,12 @@ wget http://wordpress.org/latest.zip;
 unzip -q latest.zip;
 mv wp-config-sample.php wp-config.php
 
+echo "Desactivando editor de archivos...";
+echo "define(‘DISALLOW_FILE_EDIT’, true);" >> wp-config.php
+
+echo "Desactivando uso de FTP para temas/plugins";
+echo "define(‘FS_METHOD’,’direct’);" >> wp-config.php
+
 echo "Descargando e instalando W3 Total Cache...";
 wget http://downloads.wordpress.org/plugin/w3-total-cache.zip
 unzip -q  w3-total-cache.zip;
@@ -16,6 +22,9 @@ mv w3-total-cache wordpress/wp-content/plugins/
 
 echo "Limpiando directorio y archivos temporales...";
 rm *.zip
+
+mv wordpress/* .;
+rm -rf wordpress;
 
 echo "Creando htaccess..."
 touch .htaccess
@@ -32,14 +41,5 @@ echo "Reparando y estableciendo permisos..."
 chown www:www .htaccess
 find . -type f -exec chmod 664 {} +
 find . -type d -exec chmod 775 {} +
-
-mv wordpress/* .;
-rm -rf wordpress;
-
-echo "Desactivando editor de archivos...";
-echo "define(‘DISALLOW_FILE_EDIT’, true);" >> wp-config.php
-
-echo "Desactivando uso de FTP para temas/plugins";
-echo "define(‘FS_METHOD’,’direct’);" >> wp-config.php
 
 echo "Finalizado!";
