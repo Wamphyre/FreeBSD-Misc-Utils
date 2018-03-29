e1b9aea99cb0beed531ac8c15222d44f41c8c540
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2018-03-29 07:35:28 +0200

    Update autofemp.sh

--- autofemp.sh
+++ autofemp.sh
@@ -36,45 +36,71 @@ touch nginx.conf;
 
 echo 'user  www;
 worker_processes  1;
+
+# This default error log path is compiled-in to make sure configuration parsing
+# errors are logged somewhere, especially during unattended boot when stderr
+# isn't normally logged anywhere. This path will be touched on every nginx
+# start regardless of error log location configured here. See
+# https://trac.nginx.org/nginx/ticket/147 for more info. 
+#
 error_log  /var/log/nginx/error.log;
+#
+
+#pid        logs/nginx.pid;
+
 
 events {
-worker_connections  1024;
+    worker_connections  1024;
 }
+
 
 http {
-include       mime.types;
-default_type  application/octet-stream;
+    include       mime.types;
+    default_type  application/octet-stream;
 
-log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-'$status $body_bytes_sent "$http_referer" '
-'"$http_user_agent" "$http_x_forwarded_for"';
-access_log /var/log/nginx/access.log;
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
 
-sendfile        on;
-keepalive_timeout  65;
+    access_log  /var/log/nginx//access.log;
 
-# Nginx cache configuration
-proxy_cache_path    /var/nginx/cache levels=1:2 keys_zone=my-cache:8m max_size=1000m inactive=600m;
-proxy_temp_path     /var/nginx/cache/tmp;
-proxy_cache_key     "$scheme$host$request_uri";
+    sendfile        on;
+    #tcp_nopush     on;
 
-gzip  on;
+    #keepalive_timeout  0;
+    keepalive_timeout  65;
 
-server {
-listen       8080;
-server_name  localhost;
+    gzip  on;
 
-location /nginx_status {
-location / {
+    server {
+        listen       8080;
+        server_name  localhost;
 
-root /usr/local/www/nginx;
-index index.php index.html index.htm
+        #charset koi8-r;
 
-}
-stub_status on;
-access_log off;
-}
+        #access_log  logs/host.access.log  main;
+
+        location / {
+            root   /usr/local/www/nginx;
+            index  index.php index.html index.htm;
+        }
+
+        #error_page  404              /404.html;
+
+        # redirect server error pages to the static page /50x.html
+        #
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   /usr/local/www/nginx-dist;
+        }
+
+        # proxy the PHP scripts to Apache listening on 127.0.0.1:80
+        #
+        #location ~ \.php$ {
+        #    proxy_pass   http://127.0.0.1;
+        #}
+
+        # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
 
 location ~ \.php$ {
 root	/usr/local/www/nginx;
@@ -83,19 +109,54 @@ fastcgi_index  index.php;
 fastcgi_param SCRIPT_FILENAME $request_filename;    
 include        fastcgi_params;
 }
+        # deny access to .htaccess files, if Apache's document root
+        # concurs with nginx's one
+        #
+        #location ~ /\.ht {
+        #    deny  all;
+        #}
+    }
 
-# redirect server error pages to the static page /50x.html
-#
-error_page   500 502 503 504  /50x.html;
-location = /50x.html {
-root   /usr/local/www/nginx-dist;
-}
 
-}
+    # another virtual host using mix of IP-, name-, and port-based configuration
+    #
+    #server {
+    #    listen       8000;
+    #    listen       somename:8080;
+    #    server_name  somename  alias  another.alias;
+
+    #    location / {
+    #        root   html;
+    #        index  index.html index.htm;
+    #    }
+    #}
+
+
+    # HTTPS server
+    #
+    #server {
+    #    listen       443 ssl;
+    #    server_name  localhost;
+
+    #    ssl_certificate      cert.pem;
+    #    ssl_certificate_key  cert.key;
+
+    #    ssl_session_cache    shared:SSL:1m;
+    #    ssl_session_timeout  5m;
+
+    #    ssl_ciphers  HIGH:!aNULL:!MD5;
+    #    ssl_prefer_server_ciphers  on;
+
+    #    location / {
+    #        root   html;
+    #        index  index.html index.htm;
+    #    }
+    #}
 
 include /usr/local/etc/nginx/vhost/*;
 
-}' >> nginx.conf;
+}
+' >> nginx.conf;
 
 mkdir -p /var/nginx/cache;
 mkdir -p /var/log/nginx/;
@@ -105,36 +166,15 @@ cd vhost;
 touch vhost_test.conf;
 
 echo 'server {
-# Replace with your FreeBSD Server IP
-listen 127.0.0.1:8080;
-
-# Document Root
-root /usr/local/www/nginx/;
-index index.php index.html index.htm;
-
-# Domain
-server_name www.mydomain.com mydomain.com;
-
-# Error and Access log file
-error_log  /var/log/nginx/mydomain-error.log;
-access_log /var/log/nginx/mydomain-access.log main;
-
-# Cache configuration
-proxy_cache my-cache;
-proxy_cache_valid 10s;
-proxy_no_cache $cookie_PHPSESSID;
-proxy_cache_bypass $cookie_PHPSESSID;
-proxy_cache_key "$scheme$host$request_uri";
-
-}
-
-# Enable Cache the file 30 days
-proxy_cache_valid 200 120m;
-expires 30d;
-proxy_cache my-cache;
-access_log off;
-
-' >> vhost_test.conf;
+        listen       127.0.0.1:8080;
+        server_name  www.mydomain.com mydomain.com;
+        location / {
+        root /usr/local/www/nginx/;
+        index  index.php index.html index.htm;
+        error_log  /var/log/nginx/mydomain-error.log;
+        access_log /var/log/nginx/mydomain-access.log main;
+        }
+}' >> vhost_test.conf;
 
 cd;
 
@@ -156,7 +196,7 @@ sysrc php_fpm_enable="YES"
 sysrc nginx_enable="YES"
 
 service mysql-server start 
-service varnish start
+service varnishd start
 service php-fpm start
 service nginx start
 service php-fpm restart
