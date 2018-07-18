013cd4385d5a6d761aae2d7d013e907e2d3a09de
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2018-07-18 17:46:42 +0200

    Selector.sh
    This script is the ultimate way to install your FreeBSD desktop.
    
    Full-pledged, functional, secure and optimized :)

---
+++ selector.sh
@@ -0,0 +1,422 @@
+#!/bin/bash
+
+#Selector by Wamphyre
+#Version 1.0
+
+test $? -eq 0 || exit 1 "Necesitas ser root para ejecutar este script"
+
+echo "Bienvenido al Script Selector"
+
+echo ""; sleep 3
+
+echo "A lo largo de este script podrás seleccionar una serie de paquetes para instalar en tu sistema FreeBSD"
+
+echo ""; sleep 3
+
+echo "AVISO¡¡ EJECUTA SOLO ESTE SCRIPT EN UN SISTEMA LIMPIO"
+
+echo ""
+
+sleep 5
+
+echo "Primero, actualicemos el gestor de paquetes y todos los paquetes del sistema"
+
+echo ""; sleep 3
+
+pkg update && pkg upgrade -y
+
+echo ""
+
+echo "Gestor de paquetes y paquetes instalados actualizados"
+
+echo ""; sleep 3
+
+echo "Actualizando Sistema Operativo"
+
+echo "" ; sleep 3
+
+freebsd-update fetch && freebsd-update install
+
+echo "" 
+
+echo "Obteniendo arbol de Ports";
+
+echo ""
+
+sleep 3
+
+portsnap fetch extract
+
+echo ""
+
+sleep 3
+
+echo "Arbol de ports extraido"
+
+echo ""
+
+sleep 3
+
+echo "Mostrando entornos de escritorio recomendados"
+
+echo ""
+
+sleep 1
+
+echo "XFCE"
+
+echo ""
+
+sleep 1
+
+echo "MATE"
+
+echo ""
+
+echo ; read -p "¿Qué entorno quieres instalar?: " de ;
+
+if [ "$de" = "xfce" ]
+
+then
+
+pkg install -y xorg slim xfce nano htop xarchiver zip rar xfce4-volumed-pulse xfce4-pulseaudio-plugin xfce4-goodies thunar-archive-plugin xfce-evolution mate-icon-theme-faenza slim-themes deadbeef vlc transmission filezilla bluefish gimp
+
+sysrc moused_enable="YES"
+
+sysrc dbus_enable="YES"
+
+sysrc hald_enable="YES"
+
+sysrc slim_enable="YES"
+
+echo ""
+
+echo ; read -p "¿Estás usando un Mac?: " Y;
+
+echo ""
+
+if [ "$Y" = "si" ]
+
+then
+
+sysrc moused_enable="NO"
+
+echo 'wsp_load="YES"' >> /boot/loader.conf
+
+echo ""
+
+echo "Driver para el touchpad de Mac cargado"
+
+sleep 1
+
+echo ""
+
+else fi
+
+cd
+
+touch .xinitrc
+
+echo 'exec xfce4-session' >> .xinitrc
+
+echo ""
+
+echo ; read -p "¿Quieres habilitar XFCE para otro usuario?: " X;
+
+echo ""
+
+if [ "$X" = "si" ]
+
+then
+
+echo ; read -p "Dime usuario: " user;
+
+touch /usr/home/$user/.xinitrc
+
+echo 'exec xfce4-session' >> /usr/home/$user/.xinitrc
+
+echo ""
+
+echo "$user habilitado"
+
+else fi
+
+echo ""
+
+elif [ "$de" = "mate" ]
+
+then
+
+pkg install -y xorg slim mate-desktop mate nano htop xarchiver zip mate-icon-theme-faenza slim-themes deadbeef vlc transmission filezilla bluefish gimp
+
+sysrc moused_enable="YES"
+
+sysrc dbus_enable="YES"
+
+sysrc hald_enable="YES"
+
+sysrc slim_enable="YES"
+
+echo ""
+
+echo ; read -p "¿Estás usando un Mac?: " Y;
+
+echo ""
+
+if [ "$Y" = "si" ]
+
+then
+
+sysrc moused_enable="NO"
+
+echo 'wsp_load="YES"' >> /boot/loader.conf
+
+echo ""
+
+echo "Driver para el touchpad de Mac cargado"
+
+sleep 1
+
+echo ""
+
+else fi
+
+cd
+
+touch .xinitrc
+
+echo 'exec mate-session' >> .xinitrc
+
+echo ""
+
+echo ; read -p "¿Quieres habilitar MATE para otro usuario?: " X;
+
+echo ""
+
+if [ "$X" = "si" ]
+
+then
+
+echo ; read -p "Dime usuario: " user;
+
+touch /usr/home/$user/.xinitrc
+
+echo 'exec mate-session' >> /usr/home/$user/.xinitrc
+
+echo ""
+
+echo "$user habilitado"
+
+else fi
+
+echo ""
+
+else
+
+echo "No se instalará ningún entorno de escritorio"
+
+fi
+
+echo ""
+
+echo "Mostrando navegadores web recomendados"
+
+echo ""
+
+echo "Chromium"
+
+echo "" ; sleep 1
+
+echo "Firefox"
+
+echo "" ; sleep 1
+
+echo "Midori"
+
+echo "" ; sleep 1
+
+echo ; read -p "¿Qué navegador web quieres instalar? " browser ;
+
+echo ""
+
+if [ "$browser" = "chromium" ]
+
+then
+
+pkg install -y chromium
+
+elif [ "$browser" = "firefox" ]
+
+then 
+
+pkg install -y firefox-esr
+
+elif [ "$browser" = "midori" ]
+
+then
+
+pkg install -y midori
+
+else
+
+echo "No se instalará ningún navegador"
+
+fi
+
+echo ""
+
+echo ; read -p "¿Quieres compilar los drivers de NVIDIA?: " gpu;
+
+echo ""
+
+if [ "$gpu" = "si" ]
+
+then
+
+echo ; read -p "¿Tienes una NVIDIA antigua o moderna?: " gpu2;
+
+if [ "$gpu2" = "antigua" ]
+
+then
+
+kldload linux.ko
+
+sysrc linux_enable="YES"
+
+cd /usr/ports/x11/nvidia-driver-340
+make install clean BATCH=yes
+cd /usr/ports/x11/nvidia-settings
+make install clean BATCH=yes
+cd /usr/ports/x11/nvidia-xconfig
+make install clean BATCH=yes
+
+echo 'nvidia_load="YES"' >> /boot/loader.conf
+
+nvidia-xconfig
+
+echo ""
+
+echo "Driver instalado"
+
+echo ""
+
+elif [ "$gpu2" = "moderna" ]
+
+then
+
+kldload linux.ko
+
+sysrc linux_enable="YES"
+
+cd /usr/ports/x11/nvidia-driver
+make install clean BATCH=yes
+cd /usr/ports/x11/nvidia-settings
+make install clean BATCH=yes
+cd /usr/ports/x11/nvidia-xconfig
+make install clean BATCH=yes
+
+echo 'nvidia-modeset_load="YES"' >> /boot/loader.conf
+
+nvidia-xconfig
+
+echo ""
+
+echo "Driver instalado"
+
+else fi
+
+else echo "No se instalarán los drivers de la GPU"
+
+fi
+
+echo "Optimizando Sistema"
+
+echo ""
+
+sleep 2
+
+sysrc ntpd_enable="YES"
+sysrc ntpdate_enable="YES"
+sysrc powerd_enable="YES"
+sysrc powerd_flags="-a hiadaptive"
+sysrc pf_enable="YES"
+sysrc pflog_enable="YES"
+sysrc clear_tmp_enable="YES"
+sysrc syslogd_flags="-ss"
+sysrc sendmail_enable="NONE"
+sysrc dumpdev="NO"
+
+echo "Sistema optimizado"
+
+sleep 2
+
+echo ""
+
+echo "Aplicando Hardening"
+
+sleep 2
+
+echo ""
+
+mv /etc/sysctl.conf /etc/sysctl.conf.bk
+
+touch /etc/sysctl.conf
+
+echo 'kern.elf64.nxstack=1' >> /etc/sysctl.conf
+echo 'sysctl security.bsd.map_at_zero=0' >> /etc/sysctl.conf
+echo 'security.bsd.see_other_uids=0' >> /etc/sysctl.conf
+echo 'security.bsd.see_other_gids=0' >> /etc/sysctl.conf
+echo 'security.bsd.unprivileged_read_msgbuf=0' >> /etc/sysctl.conf
+echo 'security.bsd.unprivileged_proc_debug=0' >> /etc/sysctl.conf
+echo 'kern.randompid=9800' >> /etc/sysctl.conf
+echo 'security.bsd.stack_guard_page=1' >> /etc/sysctl.conf
+echo 'net.inet.udp.blackhole=1' >> /etc/sysctl.conf
+echo 'net.inet.tcp.blackhole=2' >> /etc/sysctl.conf
+
+sleep 1
+
+echo ".....25%"
+
+sleep 2
+
+echo "..............35%"
+
+sleep 2
+
+echo "........................75%"
+
+sleep 2
+
+echo "..................................100%"
+
+sleep 2
+
+echo ""
+
+echo "Sistema Securizado"
+
+echo ""
+
+echo "Limpiando Sistema"
+
+echo ""
+
+sleep 1
+
+pkg clean -y
+
+echo ""
+
+echo "Sistema limpio"
+
+echo ""
+
+echo "Por favor, reinicia ahora tu sistema"
+
+echo ""
+
+echo "Selector by Wamphyre :)"
+
+echo ""
+
+echo "https://wamphyre.tk"
