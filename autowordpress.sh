17edc5b6170003f47a2faef930cfbf178fad2baa
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2018-09-06 11:29:51 +0200

    Update autowordpress.sh

--- autowordpress.sh
+++ autowordpress.sh
@@ -8,11 +8,6 @@ echo "Descargando Wordpress...";
 wget http://wordpress.org/latest.zip;
 unzip -q latest.zip;
 
-echo "Descargando e instalando W3 Total Cache...";
-wget http://downloads.wordpress.org/plugin/w3-total-cache.zip
-unzip -q  w3-total-cache.zip;
-mv w3-total-cache wordpress/wp-content/plugins/
-
 echo "Limpiando directorio y archivos temporales...";
 rm *.zip
 
@@ -31,21 +26,18 @@ RewriteRule . /index.php [L]
 </IfModule>' >> .htaccess
 
 echo "Reparando y estableciendo permisos..."
+
 chown www:www .htaccess
-chown -R www:www /usr/local/www/apache24/data/*
+chown -R www:www /usr/local/www/public_html/
+chown -R www:www /usr/local/www/public_html/*
 find . -type f -exec chmod 664 {} +
 find . -type d -exec chmod 775 {} +
 
 mv wp-config-sample.php wp-config.php
 
 echo "Desactivando editor de archivos...";
-echo "
-/* Disable the file editor */
-define(‘DISALLOW_FILE_EDIT’, true);" >> wp-config.php
+echo 'define(‘DISALLOW_FILE_EDIT’, true);' >> wp-config.php
 
-echo "Desactivando uso de FTP para temas/plugins...";
-echo "
-/* Disable the FTP */
-define(‘FS_METHOD’,’direct’);" >> wp-config.php
+echo 'define(‘FS_METHOD’,’direct’);' >> wp-config.php
 
 echo "Finalizado!";
