db64bd202ca91de2e64028ce6053864458c6be35
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2018-11-01 22:59:14 +0100

    Installs Let's Encrypt SSL to domain with HSTS

---
+++ autossl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+clear
+
+echo "AutoSSL Installer"
+
+sleep 1
+
+echo ""
+
+echo ; read -p "Dime dominio a instalar el certificado SSL: " DOMINIO;
+
+echo ""
+
+certbot-3.6 --nginx -d $DOMINIO -d www.$DOMINIO
+
+echo ""
+
+service nginx restart
+
+echo ""
+
+certbot-3.6 enhance --hsts -d $DOMINIO
+
+echo ""
+
+echo "Completado. Espera unos minutos y revisa tu dominio"
