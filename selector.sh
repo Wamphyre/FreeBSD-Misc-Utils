421e57f27fe123243c13ca3e75e2fa8d6fe6c5f9
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2018-12-13 01:43:45 +0100

    Update selector.sh
    Added transmission-qt5

--- selector.sh
+++ selector.sh
@@ -79,7 +79,7 @@ if [ "$de" = "xfce" ]
 
 then
 
-pkg install -y xorg slim xfce git nano htop xarchiver zip rar xfce4-mixer thunar-archive-plugin xfce-evolution mate-icon-theme-faenza slim-freebsd-black-theme vlc transmission filezilla bluefish gimp gnome-keyring shotwell gnome-screenshot gnome-font-viewer
+pkg install -y xorg slim xfce git nano htop xarchiver zip rar xfce4-mixer thunar-archive-plugin xfce-evolution mate-icon-theme-faenza slim-freebsd-black-theme vlc transmission-qt5 filezilla bluefish gimp gnome-keyring shotwell gnome-screenshot gnome-font-viewer
 
 sysrc moused_enable="YES"
 
