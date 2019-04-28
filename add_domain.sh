ca17665f2d830020fbaabca4204dba895b2f4b5f
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2019-04-28 15:57:04 +0200

    Update add_domain.sh

--- add_domain.sh
+++ add_domain.sh
@@ -16,8 +16,6 @@ mkdir /usr/local/www/public_html/$DOMINIO
 
 chown -R www:www /usr/local/www/public_html/$DOMINIO
 
-ln -s /usr/local/www/phpMyAdmin/ /usr/local/www/public_html/$DOMINIO/phpmyadmin
-
 echo ""
 
 echo "
@@ -30,21 +28,6 @@ server_name $DOMINIO;
 
 root /usr/local/www/public_html/$DOMINIO;
 index index.php;
-}
-
-# HTTPS (port 443) server - our website
-server {
-    # listening socket that will bind to port 443 on all available IPv4 addresses
-    listen                     443 ssl http2;
-
-    # listening socket that will bind to port 443 on all available IPv6 addresses
-    listen                     [::]:443 ssl;
-
-    root                       /usr/local/www/public_html/$DOMINIO;
-    index                      index.php;
-
-    # change this to your domain name (domain.com) or host name (blog.domain.com)
-    server_name                $DOMINIO;  
     
     # DNS resolver - you may want to change it to some other provider,
     # e.g. OpenDNS: 208.67.222.222
