f0c21db565223862f569e5d0d095c8e063e0f618
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2019-05-14 21:32:00 +0200

    Create create_ftp_user.sh

---
+++ create_ftp_user.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+clear
+
+sysrc ftpd_enable="YES"
+
+echo ""
+
+service ftpd start
+
+echo ""
+
+echo ; read -p "Dime nombre de usuario FTP a crear: " USER;
+
+echo ""
+
+pw groupadd ftpgroup -g 2001
+adduser -w random -g ftpgroup -u 2001 -g 2001 -s nologin -d /usr/local/www
+
+echo ""
+
+touch /etc/ftpchroot
+
+echo "
+$USER
+@ftpgroup
+$USER	/usr/local/www/./public_html
+@	public_html
+" >> /etc/ftpchroot
+
+echo "Usuario $USER creado"
+
+echo ""
+
+service ftpd restart
+
+echo ""
+
+echo "Guarda la contraseña a buen recaudo"
