3237a1e1921c28bf0996e34bd0f82a896ab2b115
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2019-05-16 14:42:56 +0200

    Update autowordpress.sh

--- autowordpress.sh
+++ autowordpress.sh
@@ -14,20 +14,8 @@ rm *.zip
 mv wordpress/* .;
 rm -rf wordpress;
 
-echo "Creando htaccess..."
-touch .htaccess
-echo '<IfModule mod_rewrite.c>
-RewriteEngine On
-RewriteBase /
-RewriteRule ^index\.php$ - [L]
-RewriteCond %{REQUEST_FILENAME} !-f
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteRule . /index.php [L]
-</IfModule>' >> .htaccess
-
 echo "Reparando y estableciendo permisos..."
 
-chown www:www .htaccess
 chown -R www:www /usr/local/www/public_html/
 chown -R www:www /usr/local/www/public_html/*
 find . -type f -exec chmod 664 {} +
