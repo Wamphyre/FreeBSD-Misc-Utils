3e7f8a559d7e35a27b1631b477d9351092184ead
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2019-06-04 12:35:49 +0200

    Update 1_crear_zona_ZFS_usuario.sh

--- 1_crear_zona_ZFS_usuario.sh
+++ 1_crear_zona_ZFS_usuario.sh
@@ -4,7 +4,7 @@ echo ; read -p "Dime nombre de usuario a crear zona ZFS: " USUARIO;
 
 echo ; read -p "Dime tamaño de la zona ZFS (ej:2G): " CUOTA;
 
-zfs create -o quota=$CUOTA zroot/usr/local/www/$USUARIO
+zfs create -o quota=$CUOTA zroot/usr/home/$USUARIO
 
 df -h | grep $USUARIO
 
@@ -12,14 +12,14 @@ echo "Zona ZFS creada"
 
 echo "Creando directorio public_html para $USUARIO"
 
-mkdir /usr/local/www/$USUARIO/public_html
+mkdir /usr/home/$USUARIO/public_html
 
 echo "Reparando permisos del directorio..."
 
-chown -R www:www /usr/local/www/$USUARIO/public_html/
-chown -R www:www /usr/local/www/$USUARIO/public_html/*
+chown -R www:www /usr/home/$USUARIO/public_html/
+chown -R www:www /usr/home/$USUARIO/public_html/*
 
-cd /usr/local/www/$USUARIO/public_html/
+cd /usr/home/$USUARIO/public_html/
 
 find . -type f -exec chmod 664 {} +
 find . -type d -exec chmod 775 {} +
